771fb79cfe376194c3680420048f1ee00765ca07
eenymini <hhl262@nyu.edu>
2015-12-02 14:14:13 -0500

    added one test for email validation

--- app/models/closet.rb
+++ app/models/closet.rb
@@ -2,4 +2,5 @@ class Closet < ActiveRecord::Base
   belongs_to :board
   has_many :closet_items
   has_many :items, through: :closet_items
+  
 end
---
+++ spec/factories/user.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :user do
+    email 'john@flatironschool.com'
+
+  end
+end
