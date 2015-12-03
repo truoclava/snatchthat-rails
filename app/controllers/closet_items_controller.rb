0be8626bdc72b857a55cab478355b99eaa18f2f4
github username <github email address>
2015-12-03 13:39:32 -0500

    item adding

--- app/controllers/closet_items_controller.rb
+++ app/controllers/closet_items_controller.rb
@@ -1,11 +1,24 @@
 class ClosetItemsController < ApplicationController
   def create
-    binding.pry
-    @closet = Closet.find_by
+    # binding.pry
+    # @closet = Closet.find_by
   end
 
   def new
-    binding.pry
+    @closet_item = ClosetItem.new(closet_item_params)
+    @closet_item.save
+    @closet = Closet.find(closet_item_params[:closet_id])
+
+    redirect_to user_closet_path(current_user ,@closet)
   end
 
+
+private
+
+def closet_item_params
+    # binding.pry
+    # closet_id = params[:closet_id]
+    params.permit(:closet_id, :item_id)
+    # params.require(:closet_item).permit(:closet_id, :item_id, controller: :closet_item)
+  end
 end
