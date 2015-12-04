0dba4d2d425fec8e0c904e6aa8060b31c28e7ba1
angamaiton <ofurmadur@icloud.com>
2015-12-04 14:58:15 -0500

    routes/schema update

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { sessions: "users/sessions" }, path: "auth", path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }
 
 
-  resources :users, only: [:show] do
+  resources :users, only: [:show, :edit, :update] do
     resources :closets
   end
 
