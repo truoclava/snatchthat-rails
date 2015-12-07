53530c7f7fdff26f66cea920e9ea98e7eebc2f1d
github username <hhl262@nyu.edu>
2015-12-07 13:45:23 -0500

    added stats page for followers

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,11 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { sessions: "users/sessions" }, path: "auth", path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }
 
 
-  resources :users, only: [:show, :edit, :update] do
+  resources :users, only: [:show, :edit, :update, :following?] do
     resources :closets
+    member do
+      get :following, :followers
+    end
   end
 
   resources :items
