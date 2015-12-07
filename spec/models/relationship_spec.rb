0c2a245505ee86fc2a72136455ee724e722dfc81
github username <hhl262@nyu.edu>
2015-12-07 00:02:02 -0500

    created tests for for follower associations

--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -1,5 +1,22 @@
-require 'rails_helper'
+require 'spec_helper'
 
-RSpec.describe Relationship, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Relationship do
+  before :each do
+    @relationship = Relationship.new(follower_id: 1, followed_id: 2)
+end
+
+  describe "#new" do 
+    it "should be valid" do
+      expect(@relationship).to be_valid
+    end
+    it "should require a follower_id" do
+      @relationship.follower_id = nil
+      expect(@relationship).to be_invalid
+    end
+    it "should require a followed_id" do
+      @relationship.followed_id = nil
+      expect(@relationship).to be_invalid
+    end
+  end
+
 end
