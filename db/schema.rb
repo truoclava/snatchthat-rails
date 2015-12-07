b0bb099811dab8a3d89150d1f4406c9ce553eced
github username <hhl262@nyu.edu>
2015-12-07 13:30:45 -0500

    added seed data

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20151207042817) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,7 +52,8 @@ ActiveRecord::Schema.define(version: 20151207042817) do
   create_table "items", force: :cascade do |t|
     t.string   "name"
     t.string   "url"
-    t.integer  "source_id"
+    t.integer  "price"
+    t.string   "source_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -63,7 +62,6 @@ ActiveRecord::Schema.define(version: 20151207042817) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
 
   create_table "relationships", force: :cascade do |t|
     t.integer  "follower_id"
@@ -75,8 +73,6 @@ ActiveRecord::Schema.define(version: 20151207042817) do
   add_index "relationships", ["followed_id"], name: "index_relationships_on_followed_id", using: :btree
   add_index "relationships", ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true, using: :btree
   add_index "relationships", ["follower_id"], name: "index_relationships_on_follower_id", using: :btree
-
-  
 
   create_table "searches", force: :cascade do |t|
     t.datetime "created_at", null: false
