4328321f83c05d80d7ecb524a992dcbb110ea1ff
github username <hhl262@nyu.edu>
2015-12-07 00:07:16 -0500

    added following association

--- app/models/user.rb
+++ app/models/user.rb
@@ -42,6 +42,7 @@ class User < ActiveRecord::Base
   has_many :active_relationships, class_name:  "Relationship",
                                   foreign_key: "follower_id",
                                   dependent:   :destroy
+  has_many :following, through: :active_relationships, source: :followed
 
   attr_accessor :login
 
