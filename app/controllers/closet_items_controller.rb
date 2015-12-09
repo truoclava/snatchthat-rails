cf40e8f46507660ad98ae2703ee019b743ea4b8b
github username <hhl262@nyu.edu>
2015-12-09 13:53:23 -0500

    added activity feed and closet feed to users page

--- app/controllers/closet_items_controller.rb
+++ app/controllers/closet_items_controller.rb
@@ -6,7 +6,9 @@ class ClosetItemsController < ApplicationController
   end
 
   def destroy
+    
     @closet_item = ClosetItem.find(params[:id])
+
     @closet_item.destroy
     track_activity(@closet_item)
     redirect_to user_closet_path(current_user, @closet_item.closet_id)
