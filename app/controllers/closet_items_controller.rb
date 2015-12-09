8ab3b321ff5f5bf601c8159461256b2b5667e9a9
github username <hhl262@nyu.edu>
2015-12-09 17:41:19 -0500

    staging

--- app/controllers/closet_items_controller.rb
+++ app/controllers/closet_items_controller.rb
@@ -8,7 +8,7 @@ class ClosetItemsController < ApplicationController
   def destroy
     
     @closet_item = ClosetItem.find(params[:id])
-track_activity(@closet_item)
+  track_activity(@closet_item)
     @closet_item.destroy
     
     redirect_to user_closet_path(current_user, @closet_item.closet_id)
